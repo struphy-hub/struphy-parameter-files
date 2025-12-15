16f7cc02a301e981525eb413480288c26fc4f876
Pititaras Attapon <pititaras@gmail.com>
2025-12-15 15:59:42 +0100

    Update parameter of Weibel instability
    update maxwellain distribution and initial distribution.
    update initial magnetic field (perturbation)
    update initial electric field

--- VlasovMaxwellOneSpecies/weibel_instability/params_weibel_instability.py
+++ VlasovMaxwellOneSpecies/weibel_instability/params_weibel_instability.py
@@ -7,7 +7,7 @@ Electric field along "x-y"
 """
 from struphy.io.options import EnvironmentOptions, BaseUnits, Time
 from struphy.geometry import domains
-from struphy.fields_background import equils
+from struphy.fields_background import equils, generic
 from struphy.topology import grids
 from struphy.io.options import DerhamOptions
 from struphy.io.options import FieldsBackground
@@ -27,6 +27,7 @@ import cunumpy as xp
 
 # setup parameters
 k = 1.25
+beta = -1e-4
 
 # environment options
 env = EnvironmentOptions(sim_folder="sim_data")
@@ -35,13 +36,13 @@ env = EnvironmentOptions(sim_folder="sim_data")
 base_units = BaseUnits()
 
 # time stepping
-time_opts = Time(dt = 0.05, Tend = 10, split_algo = "LieTrotter")
+time_opts = Time(dt = 0.05, Tend = 100, split_algo = "LieTrotter")
 
 # geometry
 domain = domains.Cuboid(r1 = 2*xp.pi/k)
 
 # fluid equilibrium (can be used as part of initial conditions)
-equil = equils.HomogenSlab(B0z = -1e-4 * xp.sin())
+equil = equils.HomogenSlab()
 
 # grid
 grid = grids.TensorProductGrid(Nel = (32,1,1))
@@ -51,11 +52,10 @@ derham_opts = DerhamOptions(p = (3,1,1))
 
 # light-weight model instance
 model = VlasovMaxwellOneSpecies()
-
 # species parameters
 model.kinetic_ions.set_phys_params(alpha = 1, epsilon = -1)
 
-loading_params = LoadingParameters(ppc = 1000)
+loading_params = LoadingParameters(ppc = 1_000)
 weights_params = WeightsParameters(control_variate = True)
 boundary_params = BoundaryParameters()
 model.kinetic_ions.set_markers(loading_params=loading_params,
@@ -64,29 +64,33 @@ model.kinetic_ions.set_markers(loading_params=loading_params,
                                bufsize = 0.4)
 model.kinetic_ions.set_sorting_boxes(boxes_per_dim = (16,1,1), do_sort = True)
 
-binplot = BinningPlot(slice='e1_v1', n_bins=(128,128), ranges=(0.0, 1.0))
-model.kinetic_ions.set_save_data(binning_plots=(binplot,))
+binplot_1 = BinningPlot(slice="e1_v1", n_bins= (128, 128), ranges= ((0.,1.), (-1.5,1.5))) #for initial velocity distribution
+binplot_2 = BinningPlot(slice = "v1", n_bins = 128, ranges = (-1.5,1.5)) # for progression of velocity and space distribution
+model.kinetic_ions.set_save_data(binning_plots=(binplot_1, binplot_2))
 
 # propagator options
 model.propagators.maxwell.options = model.propagators.maxwell.Options()
 model.propagators.push_eta.options = model.propagators.push_eta.Options()
 model.propagators.push_vxb.options = model.propagators.push_vxb.Options(b2_var=model.em_fields.b_field)
 model.propagators.coupling_va.options = model.propagators.coupling_va.Options()
-model.initial_poisson.options = model.initial_poisson.Options()
+model.initial_poisson.options = model.initial_poisson.Options(stab_mat="M0")
 
 # background, perturbations and initial conditions
-model.em_fields.phi.add_background(FieldsBackground())
-model.em_fields.phi.add_perturbation(perturbations.TorusModesCos())
-maxwellian_1 = maxwellians.Maxwellian3D(n=(1.0, None))
-maxwellian_2 = maxwellians.Maxwellian3D(n=(0.1, None))
-background = maxwellian_1 + maxwellian_2
-model.kinetic_ions.var.add_background(background)
+model.em_fields.b_field.add_perturbation(perturbation = perturbations.ModesCos(amps=(beta,), ls = (1,)))
+
+vth1_background_val = 0.02/xp.sqrt(2)
+vth2_background_val = vth1_background_val * xp.sqrt(12)
+maxwellian = maxwellians.Maxwellian3D(n=(1.0, None), 
+                    vth1= (vth1_background_val, None) , vth2= (vth2_background_val, None)
+                )
+model.kinetic_ions.var.add_background(maxwellian)
 
 # if .add_initial_condition is not called, the background is the kinetic initial condition
-perturbation = perturbations.TorusModesCos()
-maxwellian_1pt = maxwellians.Maxwellian3D(n=(1.0, perturbation))
-init = maxwellian_1pt + maxwellian_2
-model.kinetic_ions.var.add_initial_condition(init)
+perturbation = perturbations.ModesCos(amps = (0,), ls = (1,))
+maxwellian_pt = maxwellians.Maxwellian3D(n=(1.0, None), 
+                vth1= (vth1_background_val, perturbation), vth2= (vth2_background_val, perturbation)
+                )
+model.kinetic_ions.var.add_initial_condition(maxwellian_pt)
 
 # optional: exclude variables from saving
 # model.kinetic_ions.var.save_data = False
