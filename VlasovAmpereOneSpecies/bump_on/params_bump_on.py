4f334693daca3443bb571bc22b4327a1c235d754
Stefan Possanner <stefan.possanner@ipp.mpg.de>
2026-02-20 15:23:49 +0100

    adapted bump on

--- VlasovAmpereOneSpecies/bump_on/params_bump_on.py
+++ VlasovAmpereOneSpecies/bump_on/params_bump_on.py
@@ -1,100 +1,150 @@
+# -----------------------------
+# Description of the simulation
+# -----------------------------
+# Please fill in a verbal description of the simulation. 
+# It will be printed at the beginning of the simulation and can be used to keep track of the different runs.
+
+description = f"""
+Nonlinear bump-on-tail instability: A kinetic plasma instability test case for the Vlasov-Ampère model.
+This test features a "bump" (localized excess) in the high-velocity tail of the electron velocity distribution.
+The bump-on-tail configuration is unstable to the generation of Langmuir waves, leading to energy transfer
+from the hot electron population to the growing wave field. This nonlinear process exhibits complex dynamics
+including mode coupling and particle trapping in the wave potential.
+This benchmark validates the particle-in-cell treatment of velocity-space instabilities and wave-particle interactions.
 """
-Parameter file of bump on instability implemented using VlasovAmpereOneSpecies model
-(perturbation wave number "k" = 0.1, initial stream velocity v1 = 3, v2 = -4.5)
-"""
 
-from struphy.io.options import EnvironmentOptions, BaseUnits, Time
-from struphy.geometry import domains
-from struphy.fields_background import equils
-from struphy.topology import grids
-from struphy.io.options import DerhamOptions
-from struphy.io.options import FieldsBackground
-from struphy.initial import perturbations
-from struphy.kinetic_background import maxwellians
-from struphy.particles.parameters import (LoadingParameters,
-                                   WeightsParameters,
-                                   BoundaryParameters,
-                                   BinningPlot,
-                                   KernelDensityPlot,
-                                   )
-from struphy import main
+# ------------------
+# Import Struphy API
+# ------------------
 
-# import model, set verbosity
-from struphy.models.kinetic import VlasovAmpereOneSpecies
+from struphy import (
+    BaseUnits,
+    DerhamOptions,
+    EnvironmentOptions,
+    FieldsBackground,
+    Simulation,
+    Time,
+    domains,
+    equils,
+    grids,
+    perturbations,
+)
 
-# environment options
+# For particles:
+from struphy import (
+    BinningPlot,
+    BoundaryParameters,
+    KernelDensityPlot,
+    LoadingParameters,
+    WeightsParameters,
+    maxwellians,
+)
+
+# ---------------------
+# Instance of the model
+# ---------------------
+
+from struphy.models import VlasovAmpereOneSpecies
+model = VlasovAmpereOneSpecies(with_B0 = False)
+
+# List all species and set their physical properties (charge and mass number, etc.)
+model.em_fields.set_species_properties()
+model.kinetic_ions.set_species_properties(alpha=1.0, epsilon=-1.0)
+
+# List all variables and decide whether to save their data
+model.em_fields.e_field.save_data = True
+model.em_fields.phi.save_data = True
+model.kinetic_ions.var.save_data = True
+
+# --------------------------
+# Instance of the simulation
+# --------------------------
+
+# Environment options
 env = EnvironmentOptions(sim_folder="sim_data")
-# 
-# units
+
+# Units
 base_units = BaseUnits()
 
-# time stepping
+# Time stepping
 time_opts = Time(dt = 0.1, Tend = 60.0, split_algo = "LieTrotter")
 
-# geometry
+# Geometry
 domain = domains.Cuboid(r1 = 62.83)
 
-# fluid equilibrium (can be used as part of initial conditions)
+# Fluid equilibrium (can be used as part of initial conditions)
 equil = None
 
-# grid
-grid = grids.TensorProductGrid(Nel = (32,1,1))
+# Grid
+grid = grids.TensorProductGrid(Nel=(32, 1, 1))
 
-# derham options
-derham_opts = DerhamOptions(p = (3,1,1))
+# Derham options
+derham_opts = DerhamOptions(p=(3, 1, 1))
 
-# light-weight model instance
-model = VlasovAmpereOneSpecies(with_B0 = False)
-# species parameters
-model.kinetic_ions.set_phys_params(alpha=1.0, epsilon=-1.0)
+# Simulation object
+sim = Simulation(
+    model=model,
+    params_path=__file__,
+    env=env,
+    base_units=base_units,
+    time_opts=time_opts,
+    domain=domain,
+    equil=equil,
+    grid=grid,
+    derham_opts=derham_opts,
+    verbose=True,
+)
 
-loading_params = LoadingParameters(ppc = 1000,moments=(0.0,0.0,0.0,3.0,1.0,1.0))
-weights_params = WeightsParameters(control_variate= True)
+# -------------------
+# Particle parameters
+# -------------------
+
+loading_params = LoadingParameters(ppc=1000, moments=(0.0, 0.0, 0.0, 3.0, 1.0, 1.0))
+weights_params = WeightsParameters(control_variate=True)
 boundary_params = BoundaryParameters()
 model.kinetic_ions.set_markers(loading_params=loading_params,
                                weights_params=weights_params,
                                boundary_params=boundary_params,
-                               bufsize = 0.4)
+                               bufsize = 0.4,
+                               )
 model.kinetic_ions.set_sorting_boxes(boxes_per_dim=(16, 1, 1), do_sort=True)
 
 binplot_1 = BinningPlot(slice="e1_v1", n_bins= (128, 128), ranges= ((0.,1.), (-10.0,10.0))) #for initial velocity distribution
 binplot_2 = BinningPlot(slice = "v1", n_bins = 128, ranges = (-10.0,10.0)) # for progression of velocity and space distribution
 model.kinetic_ions.set_save_data(binning_plots=(binplot_1, binplot_2))
 
-# propagator options
+# ------------------
+# Propagator options
+# ------------------
+
 model.propagators.push_eta.options = model.propagators.push_eta.Options() 
 if model.with_B0:
     model.propagators.push_vxb.options = model.propagators.push_vxb.Options()
-
 model.propagators.coupling_va.options = model.propagators.coupling_va.Options()
 model.initial_poisson.options = model.initial_poisson.Options(stab_mat="M0")
 
-# background and initial conditions
+# ------------------
+# Initial conditions
+# ------------------
+# Initial conditions are the sum of the background(s) and the perturbation(s).
+# If backgrounds or perturbations are not specified, they are assumed to be zero.
 
+# For kinetic species the background is mandatory.
+# For kinetic species, if add_initial_condition() is not called, the background is taken as the kinetic initial condition.
+# For kinetic species the perturbations are added to the moments of the distribution function (defined as tuples).
+
+# Background for kinetic species
 maxwellian_1 = maxwellians.Maxwellian3D(n=(9/10, None), u1 = (3.0, None))
 maxwellian_2 = maxwellians.Maxwellian3D(n=(1/10, None), u1 = (-4.5, None), vth1 = (0.5, None)) 
 background = maxwellian_1 + maxwellian_2
 model.kinetic_ions.var.add_background(background)
 
-# if .add_initial_condition is not called, the background is the kinetic initial condition
+# Perturbations for (some) kinetic species
 perturbation = perturbations.ModesCos(amps = (0.05,), ls = (1,))
 init1 = maxwellians.Maxwellian3D(n=(9/10, None), u1 = (3.0, None))
-init2 = maxwellians.Maxwellian3D(n = (1/10,perturbation), u1 = (-4.5,None), vth1 = (0.5,None))
+init2 = maxwellians.Maxwellian3D(n = (1/10, perturbation), u1 = (-4.5, None), vth1 = (0.5, None))
 init = init1 + init2
 model.kinetic_ions.var.add_initial_condition(init)
 
 if __name__ == "__main__":
-    # start run
-    verbose = True
-
-    main.run(model, 
-             params_path=__file__,
-             env=env,
-             base_units=base_units,
-             time_opts=time_opts,
-             domain=domain,
-             equil=equil,
-             grid=grid,
-             derham_opts=derham_opts,
-             verbose=verbose,
-             )
+    sim.run(verbose=True)
